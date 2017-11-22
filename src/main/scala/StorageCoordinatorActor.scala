cc1b6be612a24ad77ec41f587ba310845aee2234
yuriy <yudzhadobin@gmail.com>
2017-11-22 17:53:59 +0300

    remove ask on complete to pipe

--- src/main/scala/StorageCoordinatorActor.scala
+++ src/main/scala/StorageCoordinatorActor.scala
@@ -1,13 +1,12 @@
 import java.util.concurrent.TimeUnit
 
-import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import akka.actor.{Actor, Props}
 import akka.util.Timeout
-import akka.pattern.ask
+import akka.pattern.{ask, pipe}
 import messages._
 
 import scala.concurrent.{ExecutionContext}
 import scala.concurrent.duration.Duration
-import scala.util.{Failure, Success}
 
 class StorageCoordinatorActor(implicit  executionContext: ExecutionContext) extends Actor with JsonSupport {
 
@@ -25,12 +24,7 @@ class StorageCoordinatorActor(implicit  executionContext: ExecutionContext) exte
         println("actor not fount")
         sender() ! "bad"
       } else {
-        val ref = refOption.get
-        val web_ref = sender()
-        ref ? message.message onComplete {
-          case Success(result: Any) => web_ref ! result
-          case Failure(e) => web_ref ! e
-        }
+        refOption.get ? message.message pipeTo sender
       }
 
     case _: GetAllActors => sender() ! context.children.toList
